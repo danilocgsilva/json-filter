6959d6c8962c5d9e7df986a51ebf05121542b3b3
Danilo Carlos <danilo.carlos@mobly.com.br>
2022-12-08 06:53:04 -0300

    Added sample object for pure array
    A class with content to be tested

---
+++ tests/PureArray.py
@@ -0,0 +1,26 @@
+class PureArray:
+
+    def get_pure_array(self):
+
+        return '''[
+    {
+        "id": 84761064,
+        "name": "addvhost_debianlike",
+        "full_name": "danilocgsilva/addvhost_debianlike"
+    },
+    {
+        "id": 207950237,
+        "name": "add_vhost_4",
+        "full_name": "danilocgsilva/add_vhost_4"
+    },
+    {
+        "id": 213596397,
+        "name": "ale",
+        "full_name": "danilocgsilva/ale"
+    },
+    {
+        "id": 246806318,
+        "name": "apache-log-to-csv",
+        "full_name": "danilocgsilva/apache-log-to-csv"
+    }
+]'''
